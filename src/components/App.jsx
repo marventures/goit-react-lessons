f502208aabe09bc62f070feaf2a4146411a0f750
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-01-30 21:11:56 +0800

    Update code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,14 +12,41 @@ export class App extends Component {
     ],
   };
 
+  // componentDidMount() invoked after a component has been rendered to the DOM
   // implement this code
   componentDidMount() {
     // If 'contacts' key in LS is not null, we write it in the 'contacts' state
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts !== null) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+
+    // else {
+    //   this.setState({
+    //     contacts: [
+    //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    //     ],
+    //   });
+    // }
+
+    console.log('componentDidMount()');
   }
 
+  // componentDidUpdate() after a component is updated
   // implement this code
-  componentDidUpdate() {
+  componentDidUpdate(_prevProps, prevState) {
     // if 'contacts' state is updated, set value to localStorage
+    const { contacts } = this.state;
+
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+
+    console.log('componentDidUpdate()');
   }
 
   addContact = newContact => {
@@ -35,6 +62,7 @@ export class App extends Component {
   };
 
   render() {
+    console.log('render()');
     const { contacts } = this.state;
     return (
       <div>
@@ -47,3 +75,26 @@ export class App extends Component {
     );
   }
 }
+
+/*
+component mounted?
+       \
+initial render()
+       \
+componentDidMount()
+  
+
+props/states are changed?/forceUpdate()
+       \
+re - render()
+
+
+component WILL remove/unmount?
+        \
+componentWillUnmount()
+
+
+component updated?
+        | 
+componentDidUpdate()
+*/
--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,16 +1,27 @@
+import { Component } from 'react';
 import css from './ContactListItem.module.css';
 
-export const ContactListItem = ({ contact, deleteContact }) => {
+export class ContactListItem extends Component {
+  //componentWillUnmount() will be called just before the element will be removed in the DOM
+  componentWillUnmount() {
+    console.log('componentWillUnmount()');
+  }
+
   // handleDelete method
-  const handleDelete = () => {
+  handleDelete = () => {
+    const { contact, deleteContact } = this.props;
     deleteContact(contact.id);
   };
 
-  return (
-    <li className={css.contactListItem}>
-      <p>{contact.name}:</p>
-      <p>{contact.number}</p>
-      <button onClick={handleDelete}>Delete</button>
-    </li>
-  );
-};
+  render() {
+    const { contact } = this.props;
+
+    return (
+      <li className={css.contactListItem}>
+        <p>{contact.name}:</p>
+        <p>{contact.number}</p>
+        <button onClick={this.handleDelete}>Delete</button>
+      </li>
+    );
+  }
+}
